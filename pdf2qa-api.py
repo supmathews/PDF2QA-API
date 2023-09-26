8582ff519af440c960b64dd8fe7609d0d475e1b0
supmathews <mathiusz@outlook.com>
2023-09-26 19:16:29 +0100

    Refined the file, added log statements

--- pdf2qa-api.py
+++ pdf2qa-api.py
@@ -3,167 +3,100 @@ from fastapi.responses import JSONResponse
 import uvicorn
 import tempfile
 import os
-import pandas as pd
+import logging
+from json import loads, dumps
 import openai
-import fitz
-import nltk
-nltk.download('punkt')
-from nltk import tokenize
+import pandas as pd
+from pdf2qaconverter import PDF2QAConverter  # PDF2QA class
 from dotenv import load_dotenv
+import time
+
 # Load the OpenAI API Key
 # Create a .env file storing your key
 load_dotenv('openai_key.env')
 
+# Log configuration
+logging.basicConfig(filename='pdf2qa.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 # Define the FastAPI instance
 app = FastAPI()    
 
-# Function to convert the pdf file to text
-def get_text_data(pdf_path: str) -> str:
-    with fitz.open(pdf_path) as doc:
-        text = ""
-        for page in doc:
-            text += page.get_text()
-        # Return the text data
-        return text
-  
-# Function to tokenize the text and return the paragraphs
-def get_paragraphs(text_data: str) -> list:
+# Function to extract the QnA from the pdf file
+def extract_qna_pairs(pdf_path: str) -> list:
+    text_data = PDF2QAConverter.get_text_data(pdf_path)
+    paragraphs = PDF2QAConverter.get_paragraphs(text_data)
 
-    # Tokenize the text by sentences
-    result = tokenize. sent_tokenize(text_data)
-
-    # Variables to store the tokenized strings and paragraphs
-    str_paragraph = ''
-    paragraphs = []
-
-    # Iterate through the tokenized sentences
-    for i in range(len(result)):
-        sentence = result[i]
-        len_para = len(tokenize.word_tokenize(str_paragraph))
-
-        if len_para < 200:
-            str_paragraph += ' ' +  sentence
-        elif len_para >= 200:
-            paragraphs.append(str_paragraph)
-            str_paragraph = ''
-            str_paragraph += ' ' + sentence
-        elif i == len(result) - 1:
-            paragraphs.append(str_paragraph)
+    qna_pairs = []
     
-    # Return the paragraphs
-    return paragraphs
-
-# Function to get the response from OpenAI's api
-def get_qna_openai(text: str) -> str:
-    try:
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt="Generate 2 descriptive questions and answers per paragraph wise: " + text,
-            temperature=0.9,
-            max_tokens=200,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        return response.choices[0].text
-    except Exception as err:
-        return JSONResponse(content={'message': str(err)})
-    
-# Function to get the Q&A pairs from the qna generated
-def parse_qa_pairs(lines: list) -> list:
-    pairs = []
-    current_q = ''
-    current_a = ''
-    for line in lines:
-        line = line.strip()
-        if line.startswith('Q'):
-            if current_q:
-                pairs.append((current_q, current_a))
-            current_q = line
-            current_a = ''
-        elif line.startswith('A'):
-            current_a += line[:] + '\n'
-  
-    if current_q:
-        pairs.append((current_q, current_a))
-    return pairs
-
-# Function to split the Question and Answwer by removing prefixes before the actual Question and Answers
-def remove_qa_prefix(text: str) -> str:
-    # Initialise the index at end of text
-    first_occurance_index = len(text)
-
-    delimiters = ['.', '-', ':']
-
-    # Finding the index of the first occurrence of any delimiter
-    for delimiter in delimiters:
-        index = text.find(delimiter)
-        if index != -1 and index < first_occurance_index:
-            first_occurance_index = index
+    # Define the api key
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+    for index, para in enumerate(paragraphs):
+        logging.info(f'Processing Paragraph {index + 1}')
+       
+        para = para.replace('\n', '')
+        qna = []
         
-    # Return the clean string
-    return text[first_occurance_index+1:].strip()
+        logging.info(f'Extracting QNA for Paragraph: {para}')
+        response = PDF2QAConverter.get_qna_openai(para)
+        logging.info(f'Extracted QNA for Paragraph: {response}')
 
-# API Endpoint to upload file
+        qna.append(response)
+
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+            logging.info('Opened the temporary file')
+            for qa in qna:
+                try:
+                    temp_file.write(qa)
+                except Exception as err:
+                    logging.info(f'{err} in Paragraph {index + 1}')
+                    #break
+
+            temp_file.flush()
+            temp_file_path = temp_file.name
+
+        try:
+            with open(temp_file_path, 'r') as temp_file:
+                lines = temp_file.readlines()
+                pairs = PDF2QAConverter.parse_qa_pairs(lines)
+                logging.info('Extracted QNA pairs')
+        except Exception as err:
+            logging.info(err)
+            #break
+
+        for q, a in pairs:
+            try:
+                qna_pairs.append({'Paragraph': para.strip(), 'Question': PDF2QAConverter.remove_qa_prefix(q), 'Answer': PDF2QAConverter.remove_qa_prefix(a)})
+            except Exception as err:
+                logging.info('QnA was an empty sequence, moving to the next set')
+                logging.info(err)
+                #break
+
+    return qna_pairs
+
+# API endpoint to upload the PDF file
 @app.post('/upload-pdf')
 async def upload_pdf(file: UploadFile):
     try:
-        # Create a temporary directory to save the uploaded file
+        logging.info('Starting PDF2QA Converter...')
         with tempfile.TemporaryDirectory() as temp_dir:
             pdf_path = os.path.join(temp_dir, file.filename)
+            logging.info(f'Created temporary directory: {temp_dir}')
 
-            # Save the uploaded PDF file to the temporary directory
             with open(pdf_path, "wb") as pdf_file:
                 pdf_file.write(file.file.read())
+                logging.info(f'Saved the PDF file in the directory {pdf_path}')
+                time.sleep(0.5)
 
-            # Convert the pdf file to text
-            text_data = get_text_data(pdf_path)
+            qna_pairs = extract_qna_pairs(pdf_path)
+
+            if not qna_pairs:
+                logging.info('No QNA pairs found in the PDF')
+                return JSONResponse(content={'message': 'No QNA pairs found in the PDF.'})
             
-            # Extract the paragraphs
-            paragraphs = get_paragraphs(text_data)
-
-            # Define the api key
-            openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-            # Create a temporary DataFrame to store the cleaned data
-            columns = ['Paragraph', 'Question', 'Answer']      ## Adding 'paragraph' column 
-            cleaned_data = pd.DataFrame(columns=columns)
-
-            for para in paragraphs:
-                # Variable to store the Q&A pairs
-                qna = []
-
-                # Get the Q&A from the OpenAI 
-                response = get_qna_openai(para)
-
-                if type(response) != str:
-                    qna.append(response)
-                else:
-                    # move to next para
-                    break
-            
-                # Create a temporary file to store the qna generated
-                with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
-                    for qa in qna:
-                        print(qa)
-                        temp_file.write(qa)
-                    temp_file.flush()
-                    temp_file_path = temp_file.name
-
-                with open(temp_file_path, 'r') as temp_file:
-                    lines = temp_file.readlines()
-                    pairs = parse_qa_pairs(lines)
-                
-                # The column values are appended accordingly to the new_rows list below
-                new_rows = []
-                for q, a in pairs:
-                    new_rows.append({'Paragraph': para, 'Question': remove_qa_prefix(q), 'Answer': remove_qa_prefix(a)})
-                
-                cleaned_data = pd.concat([cleaned_data, pd.DataFrame(new_rows)], ignore_index=True)
-
-            return JSONResponse(content={'message': 'PDF successfully uploaded, saved and extracted!', 'paragraphs': paragraphs})
+            logging.info(f'PDF successfully uploaded, saved and extracted!')
+            return JSONResponse(content={'message': 'PDF successfully uploaded, saved, and QNA pairs extracted!', 'data': qna_pairs})
     except Exception as err:
-        return JSONResponse(content={'message': 'upload_pdf function error\n' + str(err)})
+        return JSONResponse(content={'message': str(err)})
     
 if __name__ == '__main__':
     uvicorn.run(app)
